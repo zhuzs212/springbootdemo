313b8c965f084b117df8417ff6a79b0b39ddc9c8
zhuzs <zhuzs212@163.com>
2020-04-22 16:26:39 +0800

    1.返回结果 全局统一包装机制

---
+++ admin/src/main/java/com/zhuzs/admin/service/UserService.java
@@ -0,0 +1,14 @@
+package com.zhuzs.admin.service;
+
+import com.zhuzs.entity.admin.User;
+import org.springframework.stereotype.Service;
+
+/**
+ * @description：user service接口
+ *
+ * @author: zhu_zishuang
+ * @date: 2020-04-22 15:27
+ */
+public interface UserService {
+    User getUser();
+}
